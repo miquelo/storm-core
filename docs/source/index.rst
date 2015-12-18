37888d6d40faad01bb28c1187a5be658dd9f6a37
Miquel Ferran <miquel.ferran.gonzalez@gmail.com>
2015-12-18 23:13:54 +0100

    Extracted from STORM module.

---
+++ docs/source/index.rst
@@ -0,0 +1,23 @@
+.. STORM-CORE documentation master file, created by
+   copying and replacing from STORM on Fri Dec 18 22:25 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to STORM-CORE's documentation!
+======================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+   
+   providers
+   applications
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ setup.py
@@ -0,0 +1,60 @@
+#
+# This file is part of STORM-CORE.
+#
+# STORM-CORE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# STORM-CORE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with STORM-CORE.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from setuptools import setup, find_packages
+
+setup(
+	name="storm-core",
+	version="0.1.0",
+	
+	author="STORM Team",
+	author_email="miquel.ferran.gonzalez@gmail.com",
+	
+	packages=find_packages("packages"),
+	namespace_packages=[
+		"storm",
+		"storm.application",
+		"storm.provider",
+		"storm.provider.platform",
+		"storm.provider.resource"
+	],
+	package_dir={
+		"": "packages"
+	},
+	install_requires={
+		"storm": [
+			"storm>=0.1.0"
+		]
+	},
+	extras_require={
+		"shell-color": [
+			"colorama>=0.3.3"
+		]
+	},
+	
+	entry_points={
+		"console_scripts": [
+			"storm=storm.application.shell:main"
+		]
+	},
+	url="http://pypi.python.org/pypi/storm_core_0.1.0/",
+	
+	license="LICENSE.txt",
+	description="STROM Core providers.",
+	long_description=open("README.md").read()
+)
+
